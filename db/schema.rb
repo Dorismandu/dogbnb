b4f0d4c65374354b897b4c9b3c911480d6d090ee
Francisco Santos <francisco.santos.lx81@gmail.com>
2021-05-04 18:14:38 +0200

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_174550) do
+ActiveRecord::Schema.define(version: 2021_05_03_151017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2021_04_29_174550) do
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +90,5 @@ ActiveRecord::Schema.define(version: 2021_04_29_174550) do
   add_foreign_key "bookings", "dogs"
   add_foreign_key "bookings", "users"
   add_foreign_key "dogs", "users"
+  add_foreign_key "reviews", "bookings"
 end
