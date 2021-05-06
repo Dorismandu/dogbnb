e8dc4e7fcaf29e96b5f9393df1a60f740023d918
Andre R Ferrer <45776359+andrerferrer@users.noreply.github.com>
2021-05-06 15:42:54 -0300

    Update map.js

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -7,15 +7,15 @@ const initShowMapOnClick = () => {
       const mainBlock = document.querySelector(".main-block");
       const map = document.querySelector(".map-container");
       console.log(event);
-      mainBlock.classList.add("d-none");
-      map.classList.remove("d-none");
+      mainBlock.style.visibility = 'hidden';
+      map.style.visibility = '';
     });
     listButton.addEventListener("click", (event) => {
       event.preventDefault();
       const mainBlock = document.querySelector(".main-block");
       const map = document.querySelector(".map-container");
-      mainBlock.classList.remove("d-none");
-      map.classList.add("d-none");
+      mainBlock.style.visibility = '';
+      map.style.visibility = 'hidden';
     });
   }
 };
